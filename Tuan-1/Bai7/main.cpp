13fd842ffc5bfd592cbc6454b0a5f38bd502c2e1
namanhishere <53372786+namanhishere@users.noreply.github.com>
2021-09-27 07:47:57 +0700

    update Tuan1/Bai7

---
+++ Tuan-1/Bai7/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int tu;
+    int mau;
+    int so_1;
+    int so_2;
+    cout << "Nhap so tu so: ";
+    cin >> tu;
+    cout << "Nhap so mau so:";
+    cin >> mau;
+
+
+    // it not working :(((
+    /*
+    so_2 = mau / tu;
+    so_1 = (tu / so_1)+1;
+    cout << "Phan so sau rut gon la: " << so_1 << "/" << so_2 << endl;
+    */
+
+
+    return 0;
+}
