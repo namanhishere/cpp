81c67cb33e1acaf13d7dc0643ee929342d71eccd
namanhishere <53372786+namanhishere@users.noreply.github.com>
2021-10-30 16:11:54 +0700

    bai2-loop
    rối quá nè

---
+++ BaiTap-Loop/Cuu chuong/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout <<"========================= Bang cuu chuong cua "<<n<<" =========================";
+    cout<<"\n";
+    cout<<"\n";
+    for(int i = 1; i <= 10;i++){
+        cout<<"     "<<n<<" x " <<i<<" = "<<n*i<<endl;
+    }
+
+}
