a61c4290721fa84d9144d24b24e6f48a524ab2e9
namanhishere <53372786+namanhishere@users.noreply.github.com>
2021-10-30 16:32:21 +0700

    up lai

--- BaiTap-Loop/cuuchuongpromax/main.cpp
+++ BaiTap-Loop/cuuchuongpromax/main.cpp
@@ -10,13 +10,13 @@ int main()
     if(n <= 0){
         cout <<"Ban co chac la ban muon ban cuu chuong cua so am khong ????"<<endl;
     }else{
-    cout <<"========================= Bang cuu chuong tu 1 toi "<<n<<" phien ban promax ========================="<<endl;
-    for(int phan_nhan = 1; phan_nhan <= 10;phan_nhan++){
-        for(int phan_doc = 1; phan_doc <= n; phan_doc++){
-            cout <<"   "<<phan_doc<<" x "<< phan_nhan<<" = "<<phan_doc*phan_nhan;
+        cout <<"========================= Bang cuu chuong tu 1 toi "<<n<<" phien ban promax ========================="<<endl;
+        for(int phan_nhan = 1; phan_nhan <= 10;phan_nhan++){
+            for(int phan_doc = 1; phan_doc <= n; phan_doc++){
+                cout <<"   "<<phan_doc<<" x "<< phan_nhan<<" = "<<phan_doc*phan_nhan;
+            }
+            cout <<"\n";
         }
-        cout <<"\n";
-    }
     }
 
 
